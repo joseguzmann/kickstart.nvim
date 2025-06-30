e1ddc088acf1f530d20cafaa5f1e4c0db9e05e9f
Jose Guzman <jg175415@gmail.com>
2025-06-30 17:26:46 -0500

    formatting better

---
+++ lua/custom/options.lua
@@ -0,0 +1,14 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.api.nvim_create_autocmd('InsertEnter', {
+  callback = function()
+    vim.opt.relativenumber = false
+  end,
+})
+
+vim.api.nvim_create_autocmd('InsertLeave', {
+  callback = function()
+    vim.opt.relativenumber = true
+  end,
+})
