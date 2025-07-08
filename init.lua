dd35c47b19152729636ce31a960a6c685c3fa26f
José Guzmán <jg175415@gmail.com>
2025-07-08 13:41:36 -0500

    improve git changes icons

--- init.lua
+++ init.lua
@@ -275,11 +275,11 @@ require('lazy').setup({
     'lewis6991/gitsigns.nvim',
     opts = {
       signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
+        add = { text = '' },
+        change = { text = '' },
+        delete = { text = '' },
         topdelete = { text = '‾' },
-        changedelete = { text = '~' },
+        changedelete = { text = '' },
       },
     },
   },
